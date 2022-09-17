dcc313b131e2b38650591e8850982ba145cf9f8e
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-09-17 00:13:03 -0700

    Hero section

--- resume/src/components/ContactButton.tsx
+++ resume/src/components/ContactButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll";
 export default function ContactButton({ onClick }: { onClick?: () => any }) {
   return (
     <Link smooth to="contact" onClick={onClick}>
-      <button className="primary">Contact Me</button>
+      <button className="primary whitespace-nowrap">Contact Me</button>
     </Link>
   );
 }
--- resume/src/components/Hero.tsx
+++ resume/src/components/Hero.tsx
@@ -38,8 +38,11 @@ function Decor({ size }: { size: number }) {
 
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center sm:items-start p-10 h-full relative md:p-40 overflow-hidden">
-      <div className="">
+    <div
+      className="grid grid-cols-1 sm:grid-cols-2 items-start sm:items-start p-10 h-full relative md:p-40 overflow-hidden"
+      style={{ paddingBottom: 0 }}
+    >
+      <div>
         <div className="flex flex-col items-start">
           <span className="opacity-50 md:text-xl font-light italic">
             - Hey there!
@@ -47,34 +50,32 @@ export default function Hero() {
           <span className="text-4xl font-bold py-3 md:text-6xl md:py-5">
             I&apos;m Ryan Byrne
           </span>
-          <span className="text-xl md:text-3xl">
+          <span className="text-xl md:text-3xl pb-10">
             Full Stack Developer & Technology Enthusiast
           </span>
         </div>
-        <p className="my-10 w-full max-w-sm hidden sm:flex font-light md:py-10">
+        <p className="pb-10 w-full max-w-sm hidden sm:flex font-light">
           Hi, my name is Ryan and I turn ideas into reality using the power of
           the internet.
         </p>
-        <div className="flex flex-col items-end sm:items-start w-full py-10 sm:p-0">
-          <div className="flex flex-col items-center sm:flex-row">
-            <ContactButton />
-            <ScrollLink to="about" smooth>
-              <div className=" cursor-pointer flex flex-row items-center whitespace-nowrap p-4">
-                <span className="p-1 rounded-full border-2 border-gray-300">
-                  <BsFillTriangleFill
-                    className="text-xs"
-                    style={{ transform: "translateX(1.5px) rotate(90deg)" }}
-                  />
-                </span>
-                <span className="pl-2">Start Tour</span>
-              </div>
-            </ScrollLink>
-          </div>
+        <div className="flex flex-col items-end sm:items-center sm:flex-row sm:pb-20">
+          <ContactButton />
+          <ScrollLink to="about" smooth>
+            <div className=" cursor-pointer flex flex-row items-center whitespace-nowrap p-4">
+              <span className="p-1 rounded-full border-2 border-gray-300">
+                <BsFillTriangleFill
+                  className="text-xs"
+                  style={{ transform: "translateX(1.5px) rotate(90deg)" }}
+                />
+              </span>
+              <span className="pl-2">Start Tour</span>
+            </div>
+          </ScrollLink>
         </div>
       </div>
 
-      <div className="absolute bottom-0 left-0 right-0 w-full h-1/2 sm:h-3/4 -translate-x-1/4 -z-10 sm:translate-x-1/4 sm:translate-y-1/3">
-        <div className="relative w-full h-full">
+      <div className="relative flex flex-col items-start justify-end w-full h-full">
+        <div className="relative w-72 h-72 sm:w-96 sm:h-96 xl:h-full xl:w-full">
           <Image
             src={HeroImage}
             alt="hero-image"
@@ -86,3 +87,54 @@ export default function Hero() {
     </div>
   );
 }
+
+// export default function Hero() {
+//   return (
+//     <div className="flex flex-col items-center sm:items-start p-10 h-full relative md:p-40 overflow-hidden">
+//       <div className="">
+//         <div className="flex flex-col items-start">
+//           <span className="opacity-50 md:text-xl font-light italic">
+//             - Hey there!
+//           </span>
+//           <span className="text-4xl font-bold py-3 md:text-6xl md:py-5">
+//             I&apos;m Ryan Byrne
+//           </span>
+//           <span className="text-xl md:text-3xl">
+//             Full Stack Developer & Technology Enthusiast
+//           </span>
+//         </div>
+//         <p className="my-10 w-full max-w-sm hidden sm:flex font-light md:py-10">
+//           Hi, my name is Ryan and I turn ideas into reality using the power of
+//           the internet.
+//         </p>
+//         <div className="flex flex-col items-end sm:items-start w-full py-10 sm:p-0">
+//           <div className="flex flex-col items-center sm:flex-row">
+//             <ContactButton />
+//             <ScrollLink to="about" smooth>
+//               <div className=" cursor-pointer flex flex-row items-center whitespace-nowrap p-4">
+//                 <span className="p-1 rounded-full border-2 border-gray-300">
+//                   <BsFillTriangleFill
+//                     className="text-xs"
+//                     style={{ transform: "translateX(1.5px) rotate(90deg)" }}
+//                   />
+//                 </span>
+//                 <span className="pl-2">Start Tour</span>
+//               </div>
+//             </ScrollLink>
+//           </div>
+//         </div>
+//       </div>
+
+//       <div className="absolute bottom-0 left-0 right-0 w-full h-1/2 sm:h-3/4 -translate-x-1/4 -z-10 sm:translate-x-1/4 sm:translate-y-1/3">
+//         <div className="relative w-full h-full">
+//           <Image
+//             src={HeroImage}
+//             alt="hero-image"
+//             layout="fill"
+//             objectFit="contain"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
