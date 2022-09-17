1bf51413e7dd40c2baad0ca8ee2a4c00dcdb10a9
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-09-16 19:44:35 -0700

    Experience section

--- resume/src/pages/index.tsx
+++ resume/src/pages/index.tsx
@@ -1,11 +1,22 @@
+import About from "@/components/About";
 import ContactForm from "@/components/ContactForm";
 import Hero from "@/components/Hero";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Hero />
+      <div
+        id="about"
+        className="w-full column center sm:p-8 bg-primary text-white relative p-2"
+      >
+        <span
+          className="origin-top-left absolute h-full bg-red-500 -top-1/4 -rotate-3 -z-10 bg-inherit"
+          style={{ width: "150%", left: "-25%" }}
+        />
+        <About />
+      </div>
       <div
         id="contact"
         className="text-white w-full column center p-2 sm:p-8 bg-primary"
